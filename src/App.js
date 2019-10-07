5d41d2f3458545d232dd26e74d6ff8c1bf6fecf9
panveel <swisshill60@gmail.com>
2019-10-07 02:04:14 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList contacts={[
+        { name: 'Victor'},
+        {name: 'Uwem'},
+        {name: 'Isaac'}
+      ]} />
+      <ContactList contacts={[
+        { name: 'Swiss'},
+        {name: 'Chima'},
+        {name: 'Emma'}
+      ]} />
     </div>
   );
 }
