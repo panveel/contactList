58767f45e537226b39c27e4f10e6533b50b5830a
panveel <swisshill60@gmail.com>
2019-10-16 19:40:44 +0100

    modification

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
+class App extends Component{
+  state = {
+    contacts :[
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      },
+      {
+        "id": "vic",
+        "name": "Victor Churchill",
+        "handle": "victorC",
+        "avatarURL": "http://localhost:5001/victor.png"
+      }
+     ]
+  }
+  removeContact = (contact) => {
+    // this.setState({
+    //   key: "vic"
+    // })
 
+    this.setState((currentState) = ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+  }
 
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+  render(){
+    return(
+      <div>
+        <ListContacts 
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+          />
+      </div>
+    )
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <ContactList contacts={[
-        { name: 'Victor'},
-        {name: 'Uwem'},
-        {name: 'Isaac'}
-      ]} />
-      <ContactList contacts={[
-        { name: 'Swiss'},
-        {name: 'Chima'},
-        {name: 'Emma'}
-      ]} />
-    </div>
-  );
-}
+
 
 export default App;
